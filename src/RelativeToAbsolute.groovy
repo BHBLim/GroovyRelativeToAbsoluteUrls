d6450e8ddaece62e5b478d636a39e63b6865337b
Basil Lim <bhb_lim@yahoo.com>
2017-04-21 13:15:24 +0800

    This should work for converting a single file with relative urls to whatever you specify for the base URI

---
+++ src/RelativeToAbsolute.groovy
@@ -0,0 +1,77 @@
+@Grab('org.jsoup:jsoup:1.6.1')
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+class OptionsInfo {
+    String inputFileLocation
+    String outputFileLocation
+    String urlPrefix = "https://something.com/"
+}
+
+OptionsInfo readArgs(args) {
+    OptionsInfo optionsInfo = new OptionsInfo()
+    CliBuilder cli = new CliBuilder(usage:'groovy RelativeToAbsolute.groovy -i "inputFile" -o "outputFile" [-p "URLPrefix"]',
+            header:'Options:')
+    cli.with {
+        h longOpt: 'help', 'Show usage information'
+        i longOpt: 'input', 'Specify input file location', args: 1, argName: 'inputFile', required:true
+        o longOpt: 'output', 'Specify output file location', args: 1, argName: 'outputFile', required:true
+        p longOpt: 'prefix', 'Specify prefix for absolute URL', args: 1, argName: 'inputFile', required:false
+    }
+
+    def options = cli.parse(args)
+    if (!options) {
+        return null
+    }
+
+    if (options.h) {
+        cli.usage()
+        return null
+    }
+
+    optionsInfo.setInputFileLocation(options.i)
+    optionsInfo.setOutputFileLocation(options.o)
+    if (options.p) {
+        optionsInfo.setUrlPrefix(options.p)
+    }
+    return optionsInfo
+}
+
+OptionsInfo optionsInfo = readArgs(args)
+if (optionsInfo == null) {
+    return null
+}
+File inputFile = new File(optionsInfo.inputFileLocation)
+File outputFile = new File(optionsInfo.outputFileLocation)
+if (!inputFile.exists()) {
+    throw new IllegalArgumentException("The input file ${optionsInfo.inputFileLocation} does not exist!")
+}
+if (outputFile.exists()) {
+    println "The output file ${optionsInfo.outputFileLocation} already exists, overwrite? [y/N]"
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+    def userInput = br.readLine()
+    if (userInput.toUpperCase() == "Y" || userInput.toUpperCase() == "YES") {
+        println "Okay, overwriting ${optionsInfo.outputFileLocation}."
+    } else {
+        println "Okay, exiting."
+        return null
+    }
+}
+Document doc = Jsoup.parse(inputFile, "UTF-8", optionsInfo.urlPrefix);
+
+Elements hrefElements = doc.select("[href]")
+hrefElements.each(){
+    String absUrl = it.absUrl("href")
+    it.attr("href", absUrl)
+}
+
+Elements srcElements = doc.select("[src]")
+srcElements.each(){
+    String absUrl = it.absUrl("src")
+    it.attr("src", absUrl)
+}
+
+outputFile.delete()
+outputFile.createNewFile()
+outputFile << doc.toString()
